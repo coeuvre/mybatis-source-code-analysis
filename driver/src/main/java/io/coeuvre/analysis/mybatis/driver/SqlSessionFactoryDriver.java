8149e9b99f7b9b2c350096c8d810d69c2330861a
Coeuvre Wong <coeuvre@gmail.com>
2019-02-13 11:29:21 +0800

    Add driver

---
+++ driver/src/main/java/io/coeuvre/analysis/mybatis/driver/SqlSessionFactoryDriver.java
@@ -0,0 +1,26 @@
+package io.coeuvre.analysis.mybatis.driver;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+import java.util.Map;
+
+public class SqlSessionFactoryDriver {
+    public void runBuildingSqlSessionFactoryFromXML() throws Exception {
+        InputStream inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        Long userId = 1L;
+        try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+            Map<String, Object> result = sqlSession.selectOne("io.coeuvre.analysis.mybatis.driver.mapper.UserMapper.findUserById", userId);
+            System.out.println(result);
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+        new SqlSessionFactoryDriver().runBuildingSqlSessionFactoryFromXML();
+    }
+}
